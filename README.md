6e3eebfb08a367c87e8e6cec9dc12b24dc0b5936
Andrea Marchetti <afmarchetti@gmail.com>
2016-08-30 09:29:01 +0200

    add cut and readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+#Wordpress Snippets Snippets
+
+A collection of Wordpress utilities by [MarchettiDesign.net](http://www.marchettidesign.net/)
+
+
+##List of the utilities
+
+- Custom Post Type
+
+
+## Usage
+Copy and paste the snippets.
+
+## Licensing
+MIT © 2016 [Andrea Marchetti](http://www.marchettidesign.net/)
---
+++ custom-post-type.php
@@ -0,0 +1,48 @@
+<?php
+	
+/* ------------------------------------------------------------------------- *
+ *   Create CUSTOM POST TYPE
+ *
+ *   Change my_cpt with the slug name of your post type
+/* ------------------------------------------------------------------------- */
+
+add_action('init', 'create_my_cpt');
+
+function create_my_cpt() {
+
+    $labels = array(
+        'name'               => __('Services' , 'fullbase-plugin'),
+        'singular_name'      => __('Service', 'fullbase-plugin'),
+        'add_new'            => __('Add Service', 'fullbase-plugin'),
+        'add_new_item'       => __('Add New Service', 'fullbase-plugin'),
+        'edit_item'          => __('Edit Service', 'fullbase-plugin'),
+        'new_item'           => __('New Service', 'fullbase-plugin'),
+        'all_items'          => __('All Services', 'fullbase-plugin'),
+        'view_item'          => __('View Service', 'fullbase-plugin'),
+        'search_items'       => __('Search Service' , 'fullbase-plugin'),
+        'not_found'          => __('Service Not found', 'fullbase-plugin'),
+        'not_found_in_trash' => __('Service Not found in the trash', 'fullbase-plugin'),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'rewrite'            => array('slug' => 'services'),
+        'has_archive'        => true,
+        'hierarchical'       => true,
+        'menu_position'      => 22,
+        'menu_icon' 		 => 'dashicons-welcome-write-blog',
+        'supports'           => array(
+                                'title',
+                                'editor',
+                                'thumbnail',
+                                'excerpt',
+                                'page-attributes'
+                                ),
+
+    );
+
+   register_post_type('my_cpt', $args);
+}
+
+?>
