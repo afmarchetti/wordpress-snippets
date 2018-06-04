eae2904653e32146ff1d07427016b3320a5c4c95
Andrea Marchetti <afmarchetti@users.noreply.github.com>
2018-06-04 11:42:15 +0200

    Update gallery-loop.php

--- gallery-loop.php
+++ gallery-loop.php
@@ -17,6 +17,22 @@
 	  return $content;
 	}
 	add_filter('the_content', 'remove_gallery');
+	// Remove gallery from content
+add_filter('the_content', 'strip_shortcode_gallery');
+function strip_shortcode_gallery( $content ) {
+    preg_match_all( '/'. get_shortcode_regex() .'/s', $content, $matches, PREG_SET_ORDER );
+    if ( ! empty( $matches ) ) {
+        foreach ( $matches as $shortcode ) {
+            if ( 'gallery' === $shortcode[2] ) {
+                $pos = strpos( $content, $shortcode[0] );
+                if ($pos !== false)
+                    return substr_replace( $content, '', $pos, strlen($shortcode[0]) );
+            }
+        }
+    }
+    return $content;
+}
+
 	*/
 	
 
