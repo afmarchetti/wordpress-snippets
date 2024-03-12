e66371bf07859b7d2de5c436b8565f792962a749
Andrea Marchetti (Marchetti Design) <afmarchetti@users.noreply.github.com>
2024-03-12 09:20:19 +0100

    Update shortcode.php

--- shortcode.php
+++ shortcode.php
@@ -54,3 +54,60 @@ function nakedbase_slider_shortcode() {
 }
 // register shortcode
 add_shortcode('slider', 'custom_slider_shortcode');
+
+/* Shortcode Custom Loop 
+------------------------------------------------------------------------- */
+
+function custom_shortcode_inevidenza() {
+
+$buffer = '<div class="grid">';
+
+$custom_loop = new WP_Query( array(
+  'post_type'     => 'immobile',
+  'tax_query' => array(
+    array(
+        'taxonomy' => 'tag-immobile',
+        'field' => 'slug',
+        'terms' => array( 'in-evidenza'),
+     )),
+  'posts_per_page' => 3,
+  'orderby'        => 'menu_order',
+  'order'          => 'ASC',
+));
+
+if ($custom_loop->have_posts()) : while($custom_loop->have_posts()) : $custom_loop->the_post(); 
+
+$image_attributes =  wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
+
+    $buffer .= '<div class="col-33 mb-3">';
+        $buffer .= '<a href="'. get_the_permalink() .'" class="container-relative">';
+            $buffer .= '<img src="'.$image_attributes[0].'" class="img-res mb-2">';
+        $buffer .= '</a>';
+        $buffer .= '<h3><a href="'. get_the_permalink(). '" class="c-dark">'. get_the_title() .'</a></h3>';
+
+
+        $buffer .= '<p>';
+            //custom terms taxonomy
+            $terms = get_the_terms( $post->ID ,'numero-bagni');
+            if($terms){
+                $buffer .= '<strong>';
+                foreach ( $terms as $term ) {
+                    $buffer .=  $term->name;
+                }
+                $buffer .= ' <small>bagni</small>';
+            } 
+            
+            // custom fields
+            if( get_field('metri_quadri') ){
+                $buffer .= ' &nbsp; '. get_field('metri_quadri').' m2';
+            }
+        $buffer .= '</p>';
+    $buffer .= '</div>';
+
+wp_reset_postdata();
+endwhile; endif;
+$buffer .= '</div>';
+
+return $buffer;
+}
+add_shortcode('immobili-inevidenza', 'custom_shortcode_inevidenza');
